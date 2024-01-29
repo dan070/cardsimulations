b119556599be62caade2a06b4de0966e141d258f
Dan Gustafsson <dan0704090017@hotmail.com>
2024-01-29 18:12:46 +0000

    tests of new functions went through

--- deckofcards/testingstuff3.py
+++ deckofcards/testingstuff3.py
@@ -6,3 +6,13 @@ mydeck.shuffle()
 
 card_to_find1 = CardDeck.Card(rank=10, suite='clubs')
 card_to_find2 = CardDeck.Card(rank=1, suite='diamonds')
+
+
+pos_of_card_1 = mydeck.find_card(card_to_find1)
+take_card_1 = mydeck.take_card(pos_of_card_1)
+print(take_card_1 == card_to_find1)
+
+pos_of_card_2 = mydeck.find_card(card_to_find2)
+take_card_2 = mydeck.take_card(pos_of_card_2)
+print(take_card_2 == card_to_find2)
+
